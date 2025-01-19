b00c8379f600eb49342a998f4b5fb7cc3e8a4a65
Tnze <cjd001113@outlook.com>
2025-01-19 19:57:04 +0800

    geiger boost improve

--- src/geiger.rs
+++ src/geiger.rs
@@ -1,28 +1,63 @@
 use cortex_m_semihosting::hprintln;
 use stm32f1xx_hal::{
-    pac::TIM4,
-    timer::{Pins, PwmChannel, PwmHz, Remap},
+    adc::Adc,
+    gpio::{Analog, ExtiPin, Pin},
+    pac::{ADC1, TIM4},
+    prelude::*,
+    timer::PwmChannel,
 };
 
-const VIN: f32 = 5000.; // mV
-const L: f32 = 15.; // mH
-const ISTA: f32 = 100.; // mA
-const TSTA: f32 = L * ISTA / VIN; // ms
+// static GEIGER_COUNT: AtomicU32 = AtomicU32::new(0);
+// static GEIGER_OUT: Mutex<OnceCell<RefCell<Pin<'B', 8>>>> = Mutex::new(OnceCell::new());
 
-pub(super) struct Geiger<const PWM_C: u8> {
+pub(super) struct Geiger<const PWM_C: u8, const OUT_P: char, const OUT_N: u8> {
     boost_pwm: PwmChannel<TIM4, PWM_C>,
+    boost_fb: Pin<'B', 0, Analog>,
+    boost_out: Pin<OUT_P, OUT_N>,
+    boost_adc: Adc<ADC1>,
+
+    count: u32,
 }
 
-impl<const PWM_C: u8> Geiger<PWM_C> {
-    fn new(boost_pwm: PwmChannel<TIM4, PWM_C>) -> Self {
-        Self { boost_pwm }
+impl<const PWM_C: u8, const OUT_P: char, const OUT_N: u8> Geiger<PWM_C, OUT_P, OUT_N> {
+    pub(super) fn new(
+        boost_pwm: PwmChannel<TIM4, PWM_C>,
+        boost_fb: Pin<'B', 0, Analog>,
+        boost_out: Pin<OUT_P, OUT_N>,
+        boost_adc: Adc<ADC1>,
+    ) -> Self {
+        Self {
+            boost_pwm,
+            boost_fb,
+            boost_out,
+            boost_adc,
+
+            count: 0,
+        }
     }
 
-    fn enable(&mut self) {
+    pub(super) fn enable(&mut self) {
+        let max = self.boost_pwm.get_max_duty();
+        let duty = (max as f32 * (1. - 0.993)) as u16;
+        self.boost_pwm.set_duty(duty);
         self.boost_pwm.enable();
+        hprintln!("duty {}, max {}", self.boost_pwm.get_duty(), max);
     }
 
-    fn feedback(&mut self, mv: u16) {
-        hprintln!("feedback: {}(mV)", mv);
+    pub(super) fn interrupt(&mut self) {
+        if self.boost_out.check_interrupt() {
+            self.boost_out.clear_interrupt_pending_bit();
+            self.count += 1;
+        }
+    }
+
+    pub(super) fn print_status(&mut self) {
+        hprintln!("Volt: {:.02}V, Count: {}", self.read_voltage(), self.count);
+    }
+
+    fn read_voltage(&mut self) -> f32 {
+        let data: u16 = self.boost_adc.read(&mut self.boost_fb).unwrap();
+        let volt = data as f32 * 1200_f32 / self.boost_adc.read_vref() as f32;
+        volt * 0.200_f32
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,7 @@
 #![no_std]
 #![feature(sync_unsafe_cell)]
 
-use core::{
-    cell::{OnceCell, RefCell, SyncUnsafeCell},
-    mem::MaybeUninit,
-    sync::atomic::{AtomicU32, Ordering},
-};
+use core::cell::{OnceCell, RefCell};
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
@@ -22,26 +18,23 @@ use critical_section::Mutex;
 use panic_halt as _;
 use stm32f1xx_hal::{
     adc,
-    gpio::{Edge, ExtiPin, Pin},
+    gpio::{Edge, ExtiPin},
     pac::{self, interrupt},
     prelude::*,
-    timer::{Channel, Tim4NoRemap},
+    timer::Tim4NoRemap,
 };
 
 mod geiger;
 
-static GEIGER_COUNT: AtomicU32 = AtomicU32::new(0);
-static GEIGER_OUT: Mutex<OnceCell<RefCell<Pin<'B', 8>>>> = Mutex::new(OnceCell::new());
+use geiger::Geiger;
+
+static GEIGER: Mutex<OnceCell<RefCell<Geiger<3, 'B', 8>>>> = Mutex::new(OnceCell::new());
 
 #[interrupt]
 fn EXTI9_5() {
     critical_section::with(|cs| {
-        if let Some(geiger_out) = GEIGER_OUT.borrow(cs).get() {
-            let geiger_out = &mut *geiger_out.borrow_mut();
-            if (*geiger_out).check_interrupt() {
-                geiger_out.clear_interrupt_pending_bit();
-                GEIGER_COUNT.fetch_add(1, Ordering::Release);
-            }
+        if let Some(geiger) = GEIGER.borrow(cs).get() {
+            geiger.borrow_mut().interrupt();
         }
     });
 }
@@ -60,58 +53,46 @@ fn main() -> ! {
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
     let mut afio = dp.AFIO.constrain();
     let mut delay = cp.SYST.delay(&clocks);
-
-    // Init Geiger Tube driver
     let mut gpiob = dp.GPIOB.split();
 
-    // OUT
-    let mut geiger_out = gpiob.pb8.into_floating_input(&mut gpiob.crh);
-    geiger_out.make_interrupt_source(&mut afio);
-    geiger_out.trigger_on_edge(&mut dp.EXTI, Edge::Falling);
-    geiger_out.enable_interrupt(&mut dp.EXTI);
-    critical_section::with(|cs| {
-        if GEIGER_OUT.borrow(cs).set(RefCell::new(geiger_out)).is_err() {
-            panic!("Failed to init GEIGER_OUT");
+    // Init Geiger Tube driver
+    {
+        let mut geiger_out = gpiob.pb8.into_floating_input(&mut gpiob.crh);
+        geiger_out.make_interrupt_source(&mut afio);
+        geiger_out.trigger_on_edge(&mut dp.EXTI, Edge::Falling);
+        geiger_out.enable_interrupt(&mut dp.EXTI);
+        let adc1 = adc::Adc::adc1(dp.ADC1, &clocks);
+        let geiger_boost_feedback = gpiob.pb0.into_analog(&mut gpiob.crl);
+        // let mut geiger_boost_out = gpiob.pb9.into_push_pull_output(&mut gpiob.crh);
+        // geiger_boost_out.set_high();
+        let geiger_boost_out = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
+        let geiger_boost_pwm = dp.TIM4.pwm_hz::<Tim4NoRemap, _, _>(
+            geiger_boost_out,
+            &mut afio.mapr,
+            4200.Hz(),
+            &clocks,
+        );
+        let mut geiger = Geiger::new(
+            geiger_boost_pwm.split(),
+            geiger_boost_feedback,
+            geiger_out,
+            adc1,
+        );
+        geiger.enable();
+        critical_section::with(|cs| {
+            if GEIGER.borrow(cs).set(RefCell::new(geiger)).is_err() {
+                panic!("Failed to init GEIGER");
+            }
+        });
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::EXTI9_5);
         }
-    });
-    unsafe {
-        pac::NVIC::unmask(pac::Interrupt::EXTI9_5);
     }
 
-    // BOOST
-    let mut adc1 = adc::Adc::adc1(dp.ADC1, &clocks);
-    let mut geiger_feedback = gpiob.pb0.into_analog(&mut gpiob.crl);
-
-    // let mut geiger_boost_out = gpiob.pb9.into_push_pull_output(&mut gpiob.crh);
-    // geiger_boost_out.set_high();
-
-    let geiger_boost_out = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
-    let mut geiger_boost_pwm =
-        dp.TIM4
-            .pwm_hz::<Tim4NoRemap, _, _>(geiger_boost_out, &mut afio.mapr, 8.kHz(), &clocks);
-
-    hprintln!("Enable CH4");
-
-    geiger_boost_pwm.set_period(4200.Hz());
-    let max = geiger_boost_pwm.get_max_duty();
-    let duty = (max as f32 * (1. - 0.993)) as u16;
-    geiger_boost_pwm.set_duty(Channel::C4, duty);
-    geiger_boost_pwm.enable(Channel::C4);
-
-    hprintln!(
-        "duty {}, max {}",
-        geiger_boost_pwm.get_duty(Channel::C4),
-        max
-    );
-
     loop {
-        let data: u16 = adc1.read(&mut geiger_feedback).unwrap();
-        let volt = data as f32 * 1200_f32 / adc1.read_vref() as f32;
-        let geiger_volt = volt * 0.200_f32;
-        hprintln!("Volt: {:.02}V", geiger_volt);
-
-        // hprintln!("wfi");
         cortex_m::asm::wfi();
-        hprintln!("Geiger Count: {}", GEIGER_COUNT.load(Ordering::Acquire));
+        critical_section::with(|cs| {
+            GEIGER.borrow(cs).get().unwrap().borrow_mut().print_status();
+        });
     }
 }
