b2f52e410ee94965585139c79fc339b0b65601e9
Tnze <cjd001113@outlook.com>
2025-01-23 12:41:46 +0800

    Add USB support

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-//! Blinks an LED
-//!
-//! This assumes that a LED is connected to pc13 as is the case on the blue pill board.
-//!
-//! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
-//! the reference manual for an explanation. This is not an issue on the blue pill.
-
 #![deny(unsafe_code)]
 // #![deny(warnings)]
 #![no_main]
 #![no_std]
 
-use panic_halt as _;
+use cortex_m_semihosting::hprintln;
+// use panic_halt as _;
+use panic_semihosting as _;
 
 mod display;
 mod geiger;
@@ -24,28 +19,68 @@ mod app {
         pac::TIM4,
         prelude::*,
         timer::{PwmChannel, Tim4NoRemap},
+        usb::{Peripheral, UsbBus, UsbBusType},
     };
+    use usb_device::prelude::*;
 
     use crate::geiger::Geiger;
 
     #[shared]
     struct Shared {
         geiger: Geiger<PwmChannel<TIM4, 3>, Pin<'B', 0, Analog>, Pin<'B', 8>>,
+        usb_dev: UsbDevice<'static, UsbBusType>,
+        serial: usbd_serial::SerialPort<'static, UsbBusType>,
     }
 
     #[local]
     struct Local {}
 
-    #[init]
+    #[init(local = [usb_bus: Option<usb_device::bus::UsbBusAllocator<UsbBusType>> = None])]
     fn init(mut ctx: init::Context) -> (Shared, Local) {
         hprintln!("hello, world");
         let mut afio = ctx.device.AFIO.constrain();
         let mut flash = ctx.device.FLASH.constrain();
         let rcc = ctx.device.RCC.constrain();
-        let clocks = rcc.cfgr.freeze(&mut flash.acr);
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.MHz())
+            .sysclk(48.MHz())
+            .pclk1(24.MHz())
+            .freeze(&mut flash.acr);
+
+        let gpioa = ctx.device.GPIOA.split();
         let mut gpiob = ctx.device.GPIOB.split();
 
+        // Init USB
+        let (usb_dev, serial) = {
+            assert!(clocks.usbclk_valid());
+            let pin_dm = gpioa.pa11;
+            let pin_dp = gpioa.pa12;
+
+            let usb = Peripheral {
+                usb: ctx.device.USB,
+                pin_dm,
+                pin_dp,
+            };
+            ctx.local.usb_bus.replace(UsbBus::new(usb));
+            let serial = usbd_serial::SerialPort::new(ctx.local.usb_bus.as_ref().unwrap());
+            let usb_dev = UsbDeviceBuilder::new(
+                ctx.local.usb_bus.as_ref().unwrap(),
+                UsbVidPid(0x16c0, 0x27dd),
+            )
+            .device_class(usbd_serial::USB_CLASS_CDC)
+            .strings(&[StringDescriptors::default()
+                .manufacturer("Fake Company")
+                .product("Serial port")
+                .serial_number("TEST")])
+            .unwrap()
+            .build();
+
+            (usb_dev, serial)
+        };
+
         // Init Geiger Tube driver
+        hprintln!("Init Geiger");
         let mut geiger = {
             let mut geiger_out = gpiob.pb8.into_floating_input(&mut gpiob.crh);
             geiger_out.make_interrupt_source(&mut afio);
@@ -64,11 +99,18 @@ mod app {
         };
         geiger.enable(&mut ctx.device.EXTI);
 
-        (Shared { geiger }, Local {})
+        (
+            Shared {
+                geiger,
+                usb_dev,
+                serial,
+            },
+            Local {},
+        )
     }
 
     #[idle]
-    fn idle(_ctx: idle::Context) -> ! {
+    fn idle(ctx: idle::Context) -> ! {
         loop {
             cortex_m::asm::wfi();
         }
@@ -82,5 +124,52 @@ mod app {
     #[task(binds = ADC1_2, shared = [geiger])]
     fn adc_eoc(mut ctx: adc_eoc::Context) {
         ctx.shared.geiger.lock(|x| x.interrupt_adc());
+    }
+
+    #[task(binds = USB_HP_CAN_TX, shared = [usb_dev, serial])]
+    fn usb_tx(ctx: usb_tx::Context) {
+        hprintln!("USB_HP_CAN_TX");
+        let mut usb_dev = ctx.shared.usb_dev;
+        let mut serial = ctx.shared.serial;
+        (&mut usb_dev, &mut serial).lock(|usb_dev, serial| {
+            super::usb_poll(usb_dev, serial);
+        });
+    }
+
+    #[task(binds = USB_LP_CAN_RX0, shared = [usb_dev, serial])]
+    fn usb_rx0(ctx: usb_rx0::Context) {
+        hprintln!("USB_LP_CAN_RX0");
+        let mut usb_dev = ctx.shared.usb_dev;
+        let mut serial = ctx.shared.serial;
+        (&mut usb_dev, &mut serial).lock(|usb_dev, serial| {
+            super::usb_poll(usb_dev, serial);
+        });
+    }
+}
+
+fn usb_poll<B: usb_device::bus::UsbBus>(
+    usb_dev: &mut usb_device::prelude::UsbDevice<'static, B>,
+    serial: &mut usbd_serial::SerialPort<'static, B>,
+) {
+    if !usb_dev.poll(&mut [serial]) {
+        hprintln!("usb_polling failed");
+        return;
+    }
+    hprintln!("usb_polled");
+
+    let mut buf = [0u8; 8];
+
+    match serial.read(&mut buf) {
+        Ok(count) if count > 0 => {
+            // Echo back in upper case
+            for c in buf[0..count].iter_mut() {
+                if 0x61 <= *c && *c <= 0x7a {
+                    *c &= !0x20;
+                }
+            }
+
+            serial.write(&buf[0..count]).ok();
+        }
+        _ => {}
     }
 }
